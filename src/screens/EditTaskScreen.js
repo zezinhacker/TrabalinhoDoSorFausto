4cecf6eb4e41878334af530fa7f6752ceff572e7
luiz <luizfelipe01700@gmail.com>
2023-07-04 14:07:24 -0300

    arrumei a página de editar tarefas

--- src/screens/EditTaskScreen.js
+++ src/screens/EditTaskScreen.js
@@ -5,34 +5,18 @@ import firestore from '@react-native-firebase/firestore';
 
 const Stack = createStackNavigator();
 
-const CreateTaskScreen = () => {
+const EditTaskScreen = ({ route, navigation }) => {
+  const { editTask } = route.params;
   const [task, setTask] = useState('');
-  const [editTask, setEditTask] = useState(null);
 
   useEffect(() => {
-    if (editTask !== null) {
-      setTask(editTask.description);
-    } else {
-      setTask('');
-    }
+    setTask(editTask.description);
   }, [editTask]);
 
-  const handleCreateTask = async () => {
-    if (editTask !== null) {
-      // Atualizar a tarefa existente
-      await firestore().collection('tasks').doc(editTask.id).update({ description: task });
-      console.log('Tarefa editada:', editTask.id, task);
-      setEditTask(null);
-    } else {
-      // Criar uma nova tarefa
-      await firestore().collection('tasks').add({ description: task });
-      console.log('Nova tarefa:', task);
-    }
-    setTask('');
-  };
-
-  const handleEditTask = (selectedTask) => {
-    setEditTask(selectedTask);
+  const handleEditTask = async () => {
+    await firestore().collection('tasks').doc(editTask.id).update({ description: task });
+    console.log('Tarefa editada:', editTask.id, task);
+    navigation.goBack();
   };
 
   return (
@@ -43,9 +27,32 @@ const CreateTaskScreen = () => {
         onChangeText={text => setTask(text)}
         style={{ marginBottom: 8, padding: 8, borderWidth: 1, borderColor: '#ccc' }}
       />
+      <Button title="Salvar Tarefa" onPress={handleEditTask} />
+    </View>
+  );
+};
+
+const CreateTaskScreen = ({ navigation }) => {
+  const [task, setTask] = useState('');
+
+  const handleCreateTask = async () => {
+    await firestore().collection('tasks').add({ description: task });
+    console.log('Nova tarefa:', task);
+    setTask('');
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', padding: 16 }}>
+      <TextInput
+        placeholder="Digite a tarefa"
+        value={task}
+        onChangeText={text => setTask(text)}
+        style={{ marginBottom: 8, padding: 8, borderWidth: 1, borderColor: '#ccc' }}
+      />
+      <Button title="Criar Tarefa" onPress={handleCreateTask} />
       <Button
-        title={editTask !== null ? 'Editar Tarefa' : 'Criar Tarefa'}
-        onPress={handleCreateTask}
+        title="Editar Tarefa"
+        onPress={() => navigation.navigate('EditTask', { editTask: { description: task } })}
       />
     </View>
   );
@@ -55,6 +62,7 @@ const App = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="CreateTask" component={CreateTaskScreen} options={{ title: 'Criar Tarefa' }} />
+      <Stack.Screen name="EditTask" component={EditTaskScreen} options={{ title: 'Editar Tarefa' }} />
     </Stack.Navigator>
   );
 };
